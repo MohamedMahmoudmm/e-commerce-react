1c0e4212e78885eed7ac4a824f28d10c518a989a
mohamed mahmoud eid <mm4574@fayoum.edu.eg>
2025-09-23 18:03:55 +0300

    orders and all product page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import ProductDetailsPage from './component/productDetails/details';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './component/homePage';
 import Dashboard from './component/adminDashboard/dashBoard';
+import OrdersList from './component/adminallorders/allorders';
+import AcceptOrder from './component/adminallorders/acceptedOrder';
+import CanceledOrder from './component/adminallorders/cancelledOrder';
+import PendingOrder from './component/adminallorders/pendingOrder';
+import AdminAllProduct from './component/adminDashboard/AdminAllProduct';
 
 
 function App() {
@@ -20,6 +25,11 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/dash" element={<Dashboard />} />
+        <Route path="/adminAllProduct" element={<AdminAllProduct />} />
+        <Route path="/orders" element={<OrdersList />} />
+        <Route path="/acceptedOrders" element={<AcceptOrder />} />
+        <Route path="/pendingOrders" element={<PendingOrder/>} />
+        <Route path="/canceledOrders" element={<CanceledOrder />} />
         <Route path="/product/:id" element={<ProductDetailsPage />} />
       </Routes>
       
---
+++ src/component/adminallorders/pendingOrder.js
@@ -0,0 +1,64 @@
+import {
+  Box,
+  Typography,
+  Grid,
+} from "@mui/material";
+import OrderCard from "./orderCard";
+
+const orders = [
+  {
+    id: "9eg76AiKmWTZgOxJPEbC",
+    date: "August 6, 2025",
+    status: "pending",
+    total: 112.99,
+    products: [
+      {
+        name: "DANVOY Womens",
+        qty: 1,
+      image: "./chair.png",
+      },
+      {
+        name: "Casual Denim Jeans",
+        qty: 2,
+      image: "./chair.png",
+      },
+    ],
+  },
+  {
+    id: "CzOKTWuR4hCUnX9UXxji",
+    date: "August 5, 2025",
+    status: "pending",
+    total: 999.99,
+    products: [
+      {
+        name: "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor",
+        qty: 1,
+      image: "./chair.png",
+      },
+      {
+        name: "Wireless Mechanical Keyboard",
+        qty: 1,
+      image: "./chair.png",
+      },
+    ],
+  },
+];
+
+
+
+export default function PendingOrder() {
+
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography variant="h5" fontWeight="bold" mb={3} textAlign="center">
+        Orders
+      </Typography>
+
+      <Grid container spacing={3} justifyContent={"center"}>
+        {orders.map((order) => (
+         <OrderCard key={order.id} order={order} />
+        ))}
+      </Grid>
+    </Box>
+  );
+}
