5e696295a445858c96ab4c1822da6bd8b133de40
zeyadashraf2002 <zezoashraf283@gmail.com>
2025-09-30 13:25:45 +0300

    updated login/signup and added protectedRoutes

--- src/App.js
+++ src/App.js
@@ -13,36 +13,35 @@ import CanceledOrder from './component/adminallorders/cancelledOrder';
 import PendingOrder from './component/adminallorders/pendingOrder';
 import AdminAllProduct from './component/adminDashboard/AdminAllProduct';
 import IlanaGrocery from './component/shop/shop';
-import Login from './component/login/Login';
-import Register from './component/register/Register';
-import WishlistPage from './component/wishlist/WishList.jsx'
+import Login from './component/Login/Login';
+import Register from './component/Register/Register';
+import WishlistPage from './component/wishlist/WishList.jsx';
 import ShoppingCart from './component/cart/cart';
 import { useEffect, useState } from 'react';
 import { initSocket } from './redux/reducers/socket';
-import ConfirmEmail from './component/Register/confirmMail.jsx';
+import ConfirmEmail from './component/register/confirmMail.jsx';
 import ResetPassword from './component/resetPass/resetPassword.jsx';
 import ForgotPassword from './component/resetPass/forgetPassword.jsx';
 import { Alert, Snackbar } from '@mui/material';
-
-
+import ProtectedRoute from './component/ProtectedRoute.js';
 
 function App() {
-  const myId = "USER_ID";   // from auth state
-  const myRole = "user";    // or "admin"
-const [snackbar, setSnackbar] = useState({
+  const myId = "USER_ID"; 
+  const myRole = "user";   
+
+  const [snackbar, setSnackbar] = useState({
     open: false,
     message: "",
     severity: "info",
   });
+
   useEffect(() => {
- const newSocket = initSocket("USER_ID", "user");
-    // Register user/admin with server
+    const newSocket = initSocket("USER_ID", "user");
     newSocket.emit("user-online", { userId: myId, role: myRole });
 
-    // Global listeners
     newSocket.on("notify-admin", (data) => {
       console.log("Admin received new order:", data);
-       setSnackbar({
+      setSnackbar({
         open: true,
         message: `New order received: ${data.orderId || ""}`,
         severity: "info",
@@ -60,53 +59,52 @@ const [snackbar, setSnackbar] = useState({
     return () => {
       newSocket.disconnect();
     };
-  }, [ myId, myRole]);
-const handleClose = () => {
+  }, [myId, myRole]);
+
+  const handleClose = () => {
     setSnackbar({ ...snackbar, open: false });
   };
+
   return (
     <div className="App">
       <BrowserRouter>
-      
-       <NavBar/>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/shop" element={<IlanaGrocery />} />
-        <Route path="/dash" element={<Dashboard />} />
-        <Route path="/adminAllProduct" element={<AdminAllProduct />} />
-        <Route path="/orders" element={<OrdersList />} />
-        <Route path="/acceptedOrders" element={<AcceptOrder />} />
-        <Route path="/pendingOrders" element={<PendingOrder/>} />
-        <Route path="/canceledOrders" element={<CanceledOrder />} />
-        <Route path="/product/:id" element={<ProductDetailsPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="/signup/confirm" element={<ConfirmEmail/>} />
-        
-        <Route path="/wishlist" element={<WishlistPage />} />
-          <Route path="/cart" element={<ShoppingCart/>} />
-      </Routes>
-       <Snackbar
-        open={snackbar.open}
-        autoHideDuration={4000}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      >
-        <Alert
-          onClose={handleClose}
-          severity={snackbar.severity}
-          sx={{ width: "100%" }}
-        >
-          {snackbar.message}
-        </Alert>
-      </Snackbar>
-    
-      
-      </BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/signup/confirm" element={<ConfirmEmail />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
 
+          <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+          <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+          <Route path="/shop" element={<ProtectedRoute><IlanaGrocery /></ProtectedRoute>} />
+          <Route path="/dash" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/adminAllProduct" element={<ProtectedRoute><AdminAllProduct /></ProtectedRoute>} />
+          <Route path="/orders" element={<ProtectedRoute><OrdersList /></ProtectedRoute>} />
+          <Route path="/acceptedOrders" element={<ProtectedRoute><AcceptOrder /></ProtectedRoute>} />
+          <Route path="/pendingOrders" element={<ProtectedRoute><PendingOrder /></ProtectedRoute>} />
+          <Route path="/canceledOrders" element={<ProtectedRoute><CanceledOrder /></ProtectedRoute>} />
+          <Route path="/wishlist" element={<ProtectedRoute><WishlistPage /></ProtectedRoute>} />
+          <Route path="/cart" element={<ProtectedRoute><ShoppingCart /></ProtectedRoute>} />
+          <Route path="/product/:id" element={<ProtectedRoute><ProductDetailsPage /></ProtectedRoute>} />
+        </Routes>
+
+        <Snackbar
+          open={snackbar.open}
+          autoHideDuration={4000}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        >
+          <Alert
+            onClose={handleClose}
+            severity={snackbar.severity}
+            sx={{ width: "100%" }}
+          >
+            {snackbar.message}
+          </Alert>
+        </Snackbar>
+      </BrowserRouter>
     </div>
   );
 }
